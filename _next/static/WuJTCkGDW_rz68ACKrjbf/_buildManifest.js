ef20fc001d5f4518373355031695d65dfb44bd3b
budlebee <bbogle7613@gmail.com>
2023-02-23 15:50:55 +0900

    done: fix my cv for academic purpose

---
+++ _next/static/WuJTCkGDW_rz68ACKrjbf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/12-9ad4288984de08bc4d35.js","static/chunks/pages/index-540562ff0d75df75dd8f.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
