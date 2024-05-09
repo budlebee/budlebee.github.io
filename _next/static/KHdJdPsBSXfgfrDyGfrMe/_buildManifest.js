ea6115ab0d9be3ada3d22614454217c44b96ad3a
budlebee <bbogle7613@gmail.com>
2024-05-09 14:25:34 +0900

    Deploy Next.js to gh-pages

---
+++ _next/static/KHdJdPsBSXfgfrDyGfrMe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/12-9ad4288984de08bc4d35.js","static/chunks/pages/index-c38cb3b86c45c0ebabce.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
